b5457fe5c9ba9f9199271472a31ba86644151873
bryanelidimas <bryanelidimas@gmail.com>
2015-06-11 13:15:51 -0500

    Image-uploading app built with carrierwave and kaminari for pagination

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order("created_at desc").page(params[:page]).per(6)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new params.require(:post).permit(:title, :photo)
+
+    if @post.save
+      redirect_to root_path, notice: "It saved!"
+    else
+      render :new
+    end
+  end
+end
